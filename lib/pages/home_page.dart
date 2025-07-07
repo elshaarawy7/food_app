f2c4aa1870081e03fc2e4074d0863fce4589a098
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-07-07 18:24:30 +0300

    add appar in home screan

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,28 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(Icons.motorcycle , size: 20,) , 
+            SizedBox(width: 10,) , 
+            Text("61 Hopper street.." , style: TextStyle(
+              color: Colors.black , 
+              fontSize: 20 , 
+              fontWeight: FontWeight.bold , 
+            ),) , 
+            SizedBox(width: 18,) , 
+            Icon(Icons.arrow_back_ios , size: 20,) , 
+            Spacer() , 
+            Image.asset("assets/logo/Vector.png") , 
+
+          ],
+        ),
+      ), 
+      body: Column(
+        
+      ),
+    );
   }
 }
