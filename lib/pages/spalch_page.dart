126eb046de453fb370383090199f330ab5badad2
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-07-07 18:08:00 +0300

    add funaction initState in spash page

--- lib/pages/spalch_page.dart
+++ lib/pages/spalch_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:food_app/pages/home_page.dart';
 
-class SpalchPage extends StatelessWidget {
+class SpalchPage extends StatefulWidget {
   const SpalchPage({super.key});
 
   @override
+  State<SpalchPage> createState() => _SpalchPageState();
+}
+
+class _SpalchPageState extends State<SpalchPage> {
+  @override 
+
+  void initState(){
+    Future.delayed(Duration(seconds: 1) , (){
+      Navigator.push(context, MaterialPageRoute(builder: (context){
+        return HomePage() ;
+      }));
+    }) ;
+  }
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Image.asset("assets/logo/Frame 43553.png")
+      ),
+    );
   }
 }
