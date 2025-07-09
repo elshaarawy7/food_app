16ae8d1d995a8871198dc1e64e9dd160158949d2
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-07-09 18:40:59 +0300

    add custem card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -189,63 +189,7 @@ class _HomePageState extends State<HomePage> {
 
           // card Widget 
 
-           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0),
-            child: Container(
-              width: double.infinity,
-              height: 77,
-              decoration: BoxDecoration(
-                color: AppColors.primary,
-                borderRadius: BorderRadius.circular(7),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: 200,
-                      child: ListView.builder(
-                          itemCount: backList.length,
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (context , index) {
-                            return Padding(
-                              padding: const EdgeInsets.only(left: 4.0),
-                              child: Container(
-                                width: 50,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  shape: BoxShape.circle,
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Image.asset(backList[index].image),
-                                ),
-                              
-                              ),
-                            );
-                          }
-                      ),
-                    ), 
-
-                     Spacer(),
-                    Container(
-                      width: 2,
-                      height: 40,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: 10),
-                    Text("View Basket",style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18
-                    )),
-                    SizedBox(width: 10),
-                    Badge(
-                        backgroundColor: Colors.red,
-                        label: Text( backList.length.toString() ,style: TextStyle(fontSize: 13)),
-                        largeSize: 13,
-                    ),
+          
                     SizedBox(width: 10), 
 
           
@@ -253,12 +197,8 @@ class _HomePageState extends State<HomePage> {
          
         ],
       ),
-    ),
-            ),
-           ),
-        ]
-      )
     );
+          
     ;
   }
 }
